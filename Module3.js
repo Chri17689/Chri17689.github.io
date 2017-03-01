eb2e232757eb013fd75ebdbab56dbb8e2400db24
Chri17689 <canderson2301@gmail.com>
2017-03-01 13:34:49 -0500

    Update Module3.js

--- Module3.js
+++ Module3.js
@@ -1,1 +1,20 @@
-
+<html>
+  <head>
+    <link rel='stylesheet' type='text/css' href='style/mod1.css'>
+    <link rel="shortcut icon" type="image/png" href="images/eddies.png"/>
+    <title>Module 1</title>
+  </head>
+  <body>
+    <img src='images/java.jpg' width='200px'>
+    <h1>Printing in Python</h1>
+    <p>
+     JavaScript is a programming language commonly used in web development. It was originally developed by Netscape as a means to add dynamic and interactive elements to websites. ... Like server-side scripting languages, such as PHP and ASP, JavaScript code can be inserted anywhere within the HTML of a webpage.
+      <a href='code/module1.py' target='_blank' >Here</a> is my practice script.
+    </p>
+    <video width="407" height="213" controls>
+      <source src="m4v/py.m4v" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+    <script src='js/mod1.js'></script>
+  </body>
+</html>
